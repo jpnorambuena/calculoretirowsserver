2c6c1af6a9a2fcada56a95d1fca1294c1b31bdf3
jpnorambuenap <jpnorambuenap@gmail.com>
2015-03-18 12:26:41 +0000

    Primera comunicación con el WS. Retorna sólo la primera salida de un total de 83. Se desconocen los motivos por los cuales no llegan las salidas restantes.

--- CalculoRetiroWSServer/src/cl/ssffaa/calculoRetiroWS/dao/interfaces/ITipoDePersonal.java
+++
@@ -1,7 +0,0 @@
-package cl.ssffaa.calculoRetiroWS.dao.interfaces;
-
-public interface ITipoDePersonal {
-	
-	public int obtenerIdTipoDePersonal(String tipoDePersonal);
-
-}
--- CalculoRetiroWSServer/src/cl/ssffaa/calculoRetiroWS/util/enums/EnumTipoCalculoReajuste.java
+++
@@ -1,8 +0,0 @@
-package cl.ssffaa.calculoRetiroWS.util.enums;
-
-public class EnumTipoCalculoReajuste {
-
-	public static  String NORMAL  = "Normal";
-	public static  String ESPECIAL  = "Especial";
-	
-}
